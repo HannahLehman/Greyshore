1058e81f4849333e88b0820554a1114e95e1d118
Hannah Lehman <hjlehman@gmail.com>
2015-06-06 21:20:17 -0400

    Build rails for site.

---
+++ greyshore_rails/app/assets/javascripts/application.js
@@ -0,0 +1,32 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require bootstrap/bootstrap
+//= require bootstrap/bootstrap.min
+//= require plugins/jquery.magnific-popup/jquery.magnific-popup
+//= require plugins/jquery.magnific-popup/jquery.magnific-popup.min
+//= require plugins/owl-carousel/owl.carousel
+//= require plugins/retina/retina
+//= require plugins/retina/retina.min
+//= require plugins/wow/wow
+//= require plugins/wow/wow.min
+//= require plugins/cbpAnimatedHeader
+//= require plugins/classie
+//= require plugins/jqBootstrapValidation
+//= require plugins/jquery.fs.wallpaper
+//= require plugins/jquery.mixitup
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require contact_me    
+//= require vitality
+//= require_tree .
